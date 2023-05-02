ce10a3a881eae754649d04443e7e9ebdab213802
joniboy74prog <joniboymataxibos74@gmail.com>
2023-05-02 14:36:28 +0200

    rajout de commentaires commande et art ASCII

--- Bar/Barman/Commande/Commande.java
+++ Bar/Barman/Commande/Commande.java
@@ -17,17 +17,50 @@ public class Commande extends UnicastRemoteObject {
         Scanner scanner = new Scanner(System.in);
 
         try{
-            DatagramSocket socket = new DatagramSocket(port);
-            DatagramPacket message = new DatagramPacket(bufferIn, buffer.length);
+            DatagramSocket socket = new DatagramSocket(port);//creation socket controle->commande
+            DatagramPacket message = new DatagramPacket(bufferIn, buffer.length);//creation packet reçu
             System.out.println("Démarrage boucle");
+            String greeting = """
+
+                                 /$$$$$$$ /$$$$$$/$$$$$$$$/$$   /$$/$$    /$$/$$$$$$$$/$$   /$$/$$   /$$        /$$$$$$ /$$   /$$       /$$$$$$$  /$$$$$$ /$$$$$$$\s
+                                | $$__  $|_  $$_| $$_____| $$$ | $| $$   | $| $$_____| $$$ | $| $$  | $$       /$$__  $| $$  | $$      | $$__  $$/$$__  $| $$__  $$
+                                | $$  \\ $$ | $$ | $$     | $$$$| $| $$   | $| $$     | $$$$| $| $$  | $$      | $$  \\ $| $$  | $$      | $$  \\ $| $$  \\ $| $$  \\ $$
+                                | $$$$$$$  | $$ | $$$$$  | $$ $$ $|  $$ / $$| $$$$$  | $$ $$ $| $$  | $$      | $$$$$$$| $$  | $$      | $$$$$$$| $$$$$$$| $$$$$$$/
+                                | $$__  $$ | $$ | $$__/  | $$  $$$$\\  $$ $$/| $$__/  | $$  $$$| $$  | $$      | $$__  $| $$  | $$      | $$__  $| $$__  $| $$__  $$
+                                | $$  \\ $$ | $$ | $$     | $$\\  $$$ \\  $$$/ | $$     | $$\\  $$| $$  | $$      | $$  | $| $$  | $$      | $$  \\ $| $$  | $| $$  \\ $$
+                                | $$$$$$$//$$$$$| $$$$$$$| $$ \\  $$  \\  $/  | $$$$$$$| $$ \\  $|  $$$$$$/      | $$  | $|  $$$$$$/      | $$$$$$$| $$  | $| $$  | $$
+                                |_______/|______|________|__/  \\__/   \\_/   |________|__/  \\__/\\______/       |__/  |__/\\______/       |_______/|__/  |__|__/  |__/
+                                                                                                                                                                  \s
+                                                                                                                                                                  \s
+                                                                                                                                                                  \s
+                                                                                                                                                                                               \s
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                                                            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣾⡇⢸⣷⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                                                                            ⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⡟⠛⠛⢻⡇⢸⡟⠛⠛⢻⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀
+                                                                            ⠀⠀⠀⠀⢀⣀⣤⣶⡟⠛⠛⠛⠛⢻⡇⢸⡟⠛⠛⠛⠛⢻⣶⣤⣀⡀⠀⠀⠀⠀
+                                                                            ⠀⠀⢸⣟⣛⣛⣛⣛⣛⣛⣛⣛⣛⣻⡇⢸⣟⣛⣛⣛⣛⣛⣛⣛⣛⣛⣻⡇⠀⠀
+                                                                            ⠀⠀⢸⣟⣉⣉⣉⣉⣉⣉⣉⣉⣉⣹⡇⢸⣏⣉⣉⣉⣉⣉⣉⣉⣉⣉⣻⡇⠀⠀
+                                                                            ⠀⢀⡈⢹⣉⣉⣉⣉⣉⣉⣉⣉⣉⣹⡇⢸⣏⣉⣉⣉⣉⣉⣉⣉⣉⣉⡏⢁⡀⠀
+                                                                            ⠀⢸⡇⢸⣍⣉⣉⣉⣉⣉⣉⣉⣉⣹⡇⢸⣏⣉⣉⣉⣉⣉⣉⣉⣉⣩⡇⢸⡇⠀
+                                                                            ⠀⠘⠃⢸⣭⣭⣭⣭⣭⣭⣭⣭⣭⣽⡇⢸⣯⣭⣭⣭⣭⣭⣭⣭⣭⣭⡇⠘⠃⠀
+                                                                            ⠀⠀⢸⣿⣥⣤⣤⣤⣤⣤⣤⣤⣤⣼⡇⢸⣧⣤⣤⣤⣤⣤⣤⣤⣤⣬⣿⡇⠀⠀
+                                                                            ⠀⠀⢸⣿⣤⣤⣤⣤⣤⣤⣤⣤⣤⣼⡇⢸⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣿⡇⠀⠀
+                                                                            ⠀⠀⢸⣷⣤⣤⣤⣤⣤⣤⣤⣤⣤⣼⡇⢸⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣾⡇⠀⠀
+                                                                            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                    
+                                        """;
+            System.out.println(greeting);
+
+
             //démarrage IHM
             while (running){
 
-            socket.receive(message);
-            InetAddress clientAddress = message.getAddress();
+            socket.receive(message);//programme bloque jusqu'à ce que le processus nous envoie un message
+            InetAddress clientAddress = message.getAddress();//recupération cordonnées du processus controle
             int clientPort = message.getPort();
             int typeBiere = Integer.parseInt(new String(message.getData(), 0, message.getLength()));
-                try {
+                try {//relier le stub (réference locale) de l'interface à l'objet implémenté chez fournisseur
                     IBiere stub = (IBiere)Naming.lookup("rmi://localhost:2020/Fournisseur");
                     switch(typeBiere){
                         case 1:
@@ -35,27 +68,28 @@ public class Commande extends UnicastRemoteObject {
                             for (Biere bir : stub.listeAmbrees()) {
                                 System.out.println(bir.getNom());
                             }
-
                             break;
-
                         case 2:
                             System.out.println("Voici la liste de nos bières Blondes");
                             for (Biere bir : stub.listeBlondes()) {
                                 System.out.println(bir.getNom());
                             }
                             break;
+                        case 3://message envoyé lorsque le processus reçoit un signal d'interruption
+                            System.out.println("Connexion Interrompue");
+                            running = false;
                         default:
                             System.out.println("Requete Invalide");
                     }
-                    String nextRequete = scanner.nextLine();
-                    Biere bir2 = stub.acheterBiere(nextRequete);
+                    String nextRequete = scanner.nextLine(); //lecture du nom de la bière qu'on souhaite commander
+                    Biere bir2 = stub.acheterBiere(nextRequete);//appel méthode remote
 
-                    while (bir2 == null){
+                    while (bir2 == null){//cas où la bière n'as pas été trouvé, on boucle jusqu'à ce que l'utilisateur indinque une disponible
                         System.out.println("La bière en question n'est pas disponible, veuillez choisir une autre");
                         nextRequete = scanner.nextLine();
                         bir2 = stub.acheterBiere(nextRequete); 
                     }
-                    buffer = nextRequete.getBytes();
+                    buffer = nextRequete.getBytes();//Renvoyer le nom de la bière achetée vers le processus controle
                     DatagramPacket response = new DatagramPacket(buffer, buffer.length, clientAddress, clientPort);
                     socket.send(response);
                 }
@@ -64,6 +98,7 @@ public class Commande extends UnicastRemoteObject {
                     running = false;
                 }
             }
+            socket.close();
         }
         catch (SocketException se){
             System.out.println(se);
